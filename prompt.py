121b0a013b364cd4dc67163f99744abed85e6e7d
Jaydin_MacBook <JaydinFreemanWork@gmail.com>
2023-07-19 19:54:40 -0400

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -4,28 +4,56 @@
 
 import openllm
 
-# To Start the openLLM server:
-# openllm server flan-t5
 
 def query_llm(prompt):
-  """
-  Query an openLLM model with a provided prompt.
-  
-  Args:
-    prompt (str): The prompt to provide to the LLM.  
-  Returns:
-    response (str): The LLM-generated response to the prompt.
-  """
-  
-  # Create an HTTPClient pointed to the openLLM server
-  client = openllm.client.HTTPClient('http://0.0.0.0:3000') 
-  
-  # Query the LLM with the provided prompt  
-  response = client.query(prompt)
-  
-  # Return the LLM's response
-  return response
+
+    # Create an HTTPClient pointed to the openLLM server
+    client = openllm.client.HTTPClient('http://0.0.0.0:3000')
+
+    # Query the LLM with the provided prompt
+    response = client.query(prompt)
+
+    # Return the LLM's response
+    return response
+
+def generate_brag_sheet(summary, name):
+
+    # Define the prompt
+    prompt = """
+    Act as an internship assistant for an intern {name} who has completed their weekly internship and sent me their day-to-day work for this week:
+
+    {summary}
+
+    Based on this, provide 5 bullet points I can add to {name}'s brag sheet that emphasize their key accomplishments, skills demonstrated, and contributions to the projects from this week in a positive and impressive way:
+    """.format(summary=summary, name=name)
+
+    # Query the LLM with the prompt
+    response = query_llm(prompt)
+
+    # Process the response to create a brag sheet
+    brag_sheet = response.split('. ')
+    brag_sheet = [brag_sheet[0]] + ['* ' + item for item in brag_sheet[1:] if item]
+
+    # Return the brag sheet
+    return '\n'.join(brag_sheet)
+
 
 # Example usage:
-prompt = "What is the meaning of life?"
-print(query_llm(prompt))
+name = "Jaydin"
+
+summary = """
+Monday: I started the week by attending a project kickoff meeting for a new client. I took detailed notes and was able to ask insightful questions about the client's needs.
+
+Tuesday: I spent the day working on a data analysis task for the new project. I used Python and pandas to clean the data and generate preliminary insights.
+
+Wednesday: I presented my initial findings to the project team. My clear communication and thorough analysis were appreciated by all team members.
+
+Thursday: I worked on improving the project's codebase. I refactored several key functions to improve readability and performance.
+
+Friday: I ended the week by documenting my work on the project. I created a detailed README file and commented my code to ensure that future team members can understand my work.
+"""
+
+brag_sheet_bullets = generate_brag_sheet(summary, name)
+
+print(brag_sheet_bullets)
+
