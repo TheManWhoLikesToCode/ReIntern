edc129e9546abd71824f1aad9e768bfeb8a78887
Jaydin_MacBook <JaydinFreemanWork@gmail.com>
2023-07-20 15:40:56 -0400

    Added Background Image

--- app.py
+++ app.py
@@ -6,18 +6,22 @@ import threading
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     return render_template('landing_page.html')
 
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
+
 @app.route("/results", methods=('GET', 'POST'))
 def result():
     return render_template('results.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
