1749da6c6a792842e78094627a6a85ab072e523d
Jaydin_MacBook <JaydinFreemanWork@gmail.com>
2023-07-24 12:13:46 -0400

    Added Generate Email & Time Quereys

--- prompt.py
+++ prompt.py
@@ -1,7 +1,6 @@
 # Author: Jaydin F
 # Date: 7/19/2023
 # Purpose: Query openLLM models like GPT-3
-
 from BingChatAPI import BingChat
 
 
@@ -17,11 +16,15 @@ def query_llm(prompt):
     return response
 
 
-def generate_weekly_email(summary, name):
-    prompt = """
+def generate_weekly_email(tasks, name):
+    # Convert the list of tasks into a string
+    tasks_str = ', '.join(
+        [f"{task['task']} on {task['date']}" for task in tasks])
+
+    prompt = f"""
     You are an AI language model. Your task is to generate a weekly email for an intern named {name} based on their weekly activities. Here are the activities:
 
-    {summary}
+    {tasks_str}
 
     Now, let's break down these activities and translate them into key accomplishments, skills demonstrated, and contributions to the projects. 
 
@@ -33,19 +36,27 @@ def generate_weekly_email(summary, name):
 
     Finally, summarize {name}'s contributions to the team and the project. 
 
-    Based on this analysis, generate a paragraph for {name}'s weekly email.
-    """.format(summary=summary, name=name)
+    Based on this analysis, generate a paragraph for {name}'s weekly email to their boss from the first person perspective.
+    
+    Remember: Only output the email.
+    """
+
+    print(f"Prompt: {prompt}")  # Debugging print statement
 
     # Query the LLM with the prompt
     response = query_llm(prompt)
+
+    print(f"Response: {response}")  # Debugging print statement
 
     # Process the response to create the email content
     email_content = response.split('. ')
     email_content = [email_content[0]] + \
         [item for item in email_content[1:] if item]
 
-    # Return the email content
-    return '\n'.join(email_content)
+    email = '\n'.join(email_content)
+
+    # Return the email
+    return email
 
 
 def generate_brag_sheet(summary, name):
@@ -98,4 +109,5 @@ def generate_brag_sheet(summary, name):
 # brag_sheet_bullets = generate_brag_sheet(summary, name)
 #
 # print(brag_sheet_bullets)
+
 print("Prompt.py ran")
--- static/script.js
+++ static/script.js
@@ -1,49 +1,57 @@
-window.onload = function () {
-  document
-    .getElementById("summaryForm")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
-      submitSummary();
-    });
-};
-
-function submitSummary() {
-  var summary = document.getElementById("dailySummary").value;
-
-  // Input validation
-  if (!summary) {
-    alert("Please enter a summary.");
-    return;
-  }
-
-  // Convert the summary to a JSON string
-  var summaryJson = JSON.stringify({
-    summary: summary,
-  });
-
-  console.log("Sending JSON: ", summaryJson);
-
-  fetch("/home", {
+function generateEmail() {
+  const summary = document.getElementById("emailContent").innerText;
+  fetch("/generate_email", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: summaryJson,
+    body: JSON.stringify({ summary: summary }),
   })
-    .then((response) => {
-      // Log the response
-      console.log("Response received: ", response);
-
-      // Read the response as JSON
-      return response.json();
-    })
+    .then((response) => response.json())
     .then((data) => {
-      // Log the response data
-      console.log("Response data: ", data);
+      console.log(data);
+      document.getElementById("emailResult").innerText = data.email_content;
+    });
+}
 
-      // Update the summaryResult paragraph with the response data
-      document.getElementById("summaryResult").innerText =
-        data.brag_sheet_bullets;
-    })
-    .catch((error) => console.error(error));
+function generateSummary() {
+  const startDate = document.getElementById("startDate").value;
+  const endDate = document.getElementById("endDate").value;
+  fetch("/generateSummary", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `start_date=${startDate}&end_date=${endDate}`,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("summaryResult").innerText = data.summary_result;
+    });
+}
+
+function addTask() {
+  const task = document.getElementById("newTask").value;
+  const date = document.getElementById("taskDate").value;
+  fetch("/addTask", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `task=${task}&date=${date}`,
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+
+function deleteTask(taskId) {
+  fetch(`/deleteTask/${taskId}`, {
+    method: "POST",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // You can add code here to update the UI based on the server's response.
+    });
 }
