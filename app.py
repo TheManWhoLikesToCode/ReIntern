9321e7a396d6c9ea728b42eaa4583cf8c777dbc8
mahxmood <mahmoodsadeghi12345@gmail.com>
2023-07-25 03:11:03 -0500

    Updating Import Statements

--- app.py
+++ app.py
@@ -275,4 +275,5 @@ def settings():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host="0.0.0.0")
+    #app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=8081)
