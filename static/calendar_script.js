1fef620ec28b06dd927bc29d0aebe3b39079ece1
Axel Cazorla <axel.cazorla10@gmail.com>
2023-07-25 19:44:41 -0400

    Calendar Working

--- static/calendar_script.js
+++ static/calendar_script.js
@@ -162,17 +162,20 @@ deleteButton.addEventListener('click', function () {
     return `${hours}:${minutes}`;
   }
 
-// Helper function to format the date and time for the server
-function formatDateTimeForServer(dateStr) {
-  const dateObj = new Date(dateStr);
-  const year = dateObj.getUTCFullYear();
-  const month = String(dateObj.getUTCMonth() + 1).padStart(2, '0');
-  const day = String(dateObj.getUTCDate()).padStart(2, '0');
-  const hours = String(dateObj.getUTCHours()).padStart(2, '0');
-  const minutes = String(dateObj.getUTCMinutes()).padStart(2, '0');
-  const seconds = String(dateObj.getUTCSeconds()).padStart(2, '0');
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-}
+  function formatDateTimeForServer(dateStr) {
+    const dateObj = new Date(dateStr);
+    
+    const year = dateObj.getFullYear();
+    const month = String(dateObj.getMonth() + 1).padStart(2, '0');
+    const day = String(dateObj.getDate()).padStart(2, '0');
+    const hours = String(dateObj.getHours()).padStart(2, '0');
+    const minutes = String(dateObj.getMinutes()).padStart(2, '0');
+    const seconds = String(dateObj.getSeconds()).padStart(2, '0');
+    
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
+  
+  
 
   // Get the "Save" button element from the edit event form
   var editEventSaveButton = document.getElementById('editEventButton');
