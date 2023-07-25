673031d9b9df52048780b59174aec891a097091f
mahxmood <mahmoodsadeghi12345@gmail.com>
2023-07-25 11:58:07 -0500

    Adding Wiki.html to Templates folder

--- BingChatAPI.py
+++ BingChatAPI.py
@@ -1,4 +1,4 @@
-from EdgeGPT import Chatbot, ConversationStyle
+from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle
 import asyncio
 from langchain.llms.base import LLM
 from typing import Optional, List, Mapping, Any
--- app.py
+++ app.py
@@ -336,4 +336,4 @@ def settings():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host="0.0.0.0", port=3000)
+    app.run(debug=True, host="0.0.0.0", port=3001)
